40cda90f0c56a24794fd708236cd8324b8603ed5
Uma <umcgorai@in.ibm.com>
2024-01-03 22:37:10 +0530

    Added controller

---
+++ springboot-azure-poc/src/main/java/com/poc/controller/AzurePocController.java
@@ -0,0 +1,17 @@
+package com.poc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("poc")
+public class AzurePocController {
+	
+	@GetMapping("/message")
+	public String getMessage () {
+		
+		return "Hello from your controller!!";
+	}
+
+}
